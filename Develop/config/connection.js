11c994d99318e74fc530bff83692c9548df713cc
Andrew Connally <ajconnally93@gmail.com>
2023-01-10 13:14:18 -0600

    push for tutoring

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// process.env.DB_NAME
+// process.env.DB_USER
+// process.env.DB_PW
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(ecommerce_db, root, root, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
@@ -12,7 +16,7 @@ const sequelize = process.env.JAWSDB_URL
       },
       
       // port added by me
-      port: 3306
+      // port:
     });
 
 module.exports = sequelize;
